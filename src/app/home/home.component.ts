16157e02341edd6b0cfffe32dad9327a37a4fc38
sajidkhan127 <sajidkhan127@gmail.com>
2020-11-26 17:14:15 +0500

    todo started

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+// import * as pouchDB from 'pouchdb';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  pouchdb: any;
+  constructor() {
+    // this.pouchdb = new pouchDB('todo');
   }
 
+  ngOnInit(): void {
+    // this.addTodo('Bill payment');
+  }
+
+  // addToDo() {
+  //   this.store.dispatch(
+  //     new AddToDo({
+  //       id: Math.random(),
+  //       complete: false,
+  //       task: this._toDo.task,
+  //     })
+  //   );
+  // }
+  // addTodo(text: any) {
+  //   var todo = {
+  //     _id: new Date().toISOString(),
+  //     title: text,
+  //     completed: false,
+  //   };
+  //   this.pouchdb.put(todo);
+  // }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { PinchZoomModule } from 'ngx-pinch-zoom';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    PinchZoomModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
